e2a6cf60040c75e85db211110ebf2c966c3b01a1
zxcpopstar <lolkeksigmakriper@gmail.com>
2024-10-18 02:16:32 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,163 +1,163 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Text.RegularExpressions;
-using System.Diagnostics;
-using System.Net.Http.Headers;
-using System.Management;
+using System.Security.Cryptography;
 
-namespace CheckStrings
+public class CheatHunterBTR
 {
-    class Program
+    private const string WEBHOOK_URL = "https://discord.com/api/webhooks/1295397605378887700/osasnvN87qmehkcFreGL33jgw-YmHdQTZ1b1idfDYb79C23lNT-oo7crS2S3ayG48Exv";
+    private const string STRINGS_FILE = "strings/strings.txt";
+    private static readonly object locker = new object();
+    private static long checkId = 0;
+    private static bool checking = false;
+
+
+    public static async Task Main(string[] args)
     {
-        static int checkNumber = 0;
-
-        // Массив шаблонов (регулярных выражений) - *важно* исправьте их!
-        private static readonly string[] patterns =
+        string[] stringsToCheck = ReadStringsFromFile(STRINGS_FILE);
+        if (stringsToCheck == null)
         {
-            @"funtime",
-            @"\vmdoqhEwxdPU",
-        };
+            Console.WriteLine($"Ошибка: файл '{STRINGS_FILE}' не найден или не может быть прочитан.");
+            return;
+        }
 
-        static void Main(string[] args)
+        Console.WriteLine("CheatHunterBTR запущен.");
+
+        while (true)
         {
+            // Запрос никнейма выносим наружу цикла
+            Console.Write("Введите никнейм для проверки (или 'exit' для выхода): ");
+            string nick = Console.ReadLine();
 
-            // Ввод ника
-            Console.Write("Введите ник: ");
-            string nickname = Console.ReadLine();
-
-            // Увеличение номера проверки
-            checkNumber++;
-            Console.WriteLine($"Номер проверки: #{checkNumber}");
-
-            // Поиск javaw.exe
-            Process javawProcess = FindJavawProcess();
-
-            if (javawProcess != null)
+            if (string.IsNullOrEmpty(nick))
             {
-                Console.WriteLine($"Найден процесс javaw: {javawProcess.ProcessName} (PID: {javawProcess.Id})");
-                Console.WriteLine("Ищем...");
-
-                SendDiscordMessage(nickname, checkNumber, patterns);
+                Console.WriteLine("Введите никнейм.");
+                continue;
             }
-            else
-            {
-                Console.WriteLine("Процесс javaw не найден.");
-                // Просим ввести PID вручную или получить его из System Informer
-                int pid = GetPidFromUserOrSystemInformer();
-                if (pid != 0)
-                {
-                    // Если PID получен успешно, пытаемся получить процесс
-                    javawProcess = Process.GetProcessById(pid);
-                    if (javawProcess != null)
-                    {
-                        Console.WriteLine($"Найден процесс javaw по PID: {pid}");
-                        Console.WriteLine("Ищем...");
 
-                        // Отправляем сообщение в Discord без проверки файла
-                        SendDiscordMessage(nickname, checkNumber, patterns);
+            if (nick.ToLower() == "exit")
+            {
+                Console.WriteLine("Чекер закрыт.");
+                return;
+            }
+
+
+            try
+            {
+                lock (locker)
+                {
+                    checkId++;
+                }
+                checking = true;
+                Console.WriteLine($"Проверка #{checkId} для {nick}...");
+                using (Process javawProcess = new Process())
+                {
+                    javawProcess.StartInfo.FileName = "javaw.exe";
+                    javawProcess.StartInfo.RedirectStandardOutput = true;
+                    javawProcess.StartInfo.UseShellExecute = false;
+                    javawProcess.StartInfo.CreateNoWindow = true;
+                    javawProcess.Start();
+
+                    var foundStrings = await CheckForStringsAsync(nick, stringsToCheck, javawProcess, TimeSpan.FromSeconds(10));
+                    checking = false;
+
+                    if (foundStrings.Count > 0)
+                    {
+                        await SendDiscordMessage($"{nick}; #{checkId}; {string.Join(", ", foundStrings)}");
                     }
                     else
                     {
-                        Console.WriteLine($"Не удалось найти процесс javaw по PID: {pid}");
+                        await SendDiscordMessage($"{nick}; #{checkId};  Ничего не найдено");
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Не удалось найти javaw по PID.");
-                }
-            }
+                    javawProcess.WaitForExit();
+                    javawProcess.Close();
 
-            Console.ReadKey();
-        }
+                }
 
-        // Метод для поиска процесса javaw.exe
-        static Process FindJavawProcess()
-        {
-            foreach (Process process in Process.GetProcesses())
-            {
-                if (process.ProcessName == "javaw")
-                {
-                    return process;
-                }
-            }
-            return null; // Процесс не найден
-        }
+                Console.Write("Введите никнейм для проверки (или 'exit' для выхода): ");
+                nick = Console.ReadLine();
 
-        // Метод для получения PID от пользователя или из System Informer
-        static int GetPidFromUserOrSystemInformer()
-        {
-            Console.Write("Введите PID javaw.exe или нажмите Enter для поиска в System Informer: ");
-            string input = Console.ReadLine();
-            if (!string.IsNullOrEmpty(input))
-            {
-                // Введен PID
-                if (int.TryParse(input, out int pid))
-                {
-                    return pid;
-                }
-                else
-                {
-                    Console.WriteLine("Некорректный формат PID.");
-                    return 0;
-                }
-            }
-            else
-            {
-                // Поиск в System Informer
-                return GetPidFromSystemInformer();
-            }
-        }
-
-        // Метод для получения PID из System Informer
-        static int GetPidFromSystemInformer()
-        {
-            try
-            {
-                // Запрос к WMI для получения информации о процессе javaw.exe
-                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT ProcessId FROM Win32_Process WHERE Name = 'javaw.exe'");
-                foreach (ManagementObject mo in searcher.Get())
-                {
-                    return (int)mo["ProcessId"];
-                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Ошибка при получении PID из System Informer: {ex.Message}");
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                checking = false;
             }
-            return 0; // PID не найден
         }
+    }
 
-        // Метод для отправки сообщения в Discord Webhook
-        static async void SendDiscordMessage(string nickname, int checkNumber, string[] patterns)
+
+    private static async Task<List<string>> CheckForStringsAsync(string nick, string[] stringsToCheck, Process javawProcess, TimeSpan timeout)
+    {
+        var foundStrings = new List<string>();
+        var cancellationTokenSource = new CancellationTokenSource(timeout);
+
+        try
         {
-            // Webhook URL
-            string webhookUrl = "https://discord.com/api/webhooks/1295397605378887700/osasnvN87qmehkcFreGL33jgw-YmHdQTZ1b1idfDYb79C23lNT-oo7crS2S3ayG48Exv";
+            using var output = javawProcess.StandardOutput;
+            string processOutput = await output.ReadToEndAsync(cancellationTokenSource.Token);
 
-            foreach (string pattern in patterns)
+            foreach (string str in stringsToCheck)
             {
-                // Проверяем, соответствуют ли шаблоны  никам в  patterns
-                bool isMatch = Regex.IsMatch(nickname, pattern);
-                if (isMatch)
+                if (Regex.IsMatch(processOutput, $"(?i){Regex.Escape(str)}", RegexOptions.IgnoreCase))
                 {
-                    using (var client = new HttpClient())
-                    {
-                        var content = new StringContent($"{nickname} ; #{checkNumber} ; {pattern}");
-                        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-                        var response = await client.PostAsync(webhookUrl, content);
-                        if (response.IsSuccessStatusCode)
-                        {
-                            Console.WriteLine("Сообщение успешно отправлено в Discord.");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Ошибка при отправке сообщения в Discord: {response.StatusCode}");
-                        }
-                    }
+                    foundStrings.Add(str);
                 }
             }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка в CheckForStringsAsync: {ex.Message}");
+            javawProcess.Kill();
+            return new List<string>();
+        }
+        return foundStrings;
+    }
+
+
+    private static string[] ReadStringsFromFile(string filePath)
+    {
+        try
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Файл '{filePath}' не найден.");
+                return null;
+            }
+
+            return File.ReadAllLines(filePath).Where(line => !string.IsNullOrWhiteSpace(line)).ToArray();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка чтения файла: {ex.Message}");
+            return null;
+        }
+    }
+
+
+    private static async Task SendDiscordMessage(string message)
+    {
+        try
+        {
+            using HttpClient client = new();
+            using HttpRequestMessage request = new(HttpMethod.Post, WEBHOOK_URL)
+            {
+                Content = new StringContent($@"{{""content"":""{message}"",""username"":""CheatHunter""}}", Encoding.UTF8, "application/json")
+            };
+
+            using HttpResponseMessage response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка отправки сообщения в Discord: {ex.Message}");
         }
     }
 }
